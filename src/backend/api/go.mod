3347efcf6f2bde24450dd0ab623f074af20d8c79
H4wk <quasand89@gmail.com>
2024-03-24 15:52:26 +0100

    add local db

--- src/backend/api/go.mod
+++ src/backend/api/go.mod
@@ -5,6 +5,8 @@ go 1.21.1
 require (
 	github.com/gin-contrib/cors v1.7.0
 	github.com/gin-gonic/gin v1.9.1
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.2.0
 )
 
 require (
--- src/backend/api/main.go
+++ src/backend/api/main.go
@@ -5,7 +5,29 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
+
+var schema = `
+CREATE TABLE IF NOT EXISTS person (
+    id serial primary key,
+    first_name text,
+    last_name text,
+    email text
+);
+`
+
+type Person struct {
+	ID        int    `db:"id"`
+	FirstName string `db:"first_name"`
+	LastName  string `db:"last_name"`
+	Email     string `db:"email"`
+}
+
+type Env struct {
+	db *sqlx.DB
+}
 
 type album struct {
 	ID     string  `json:"id"`
@@ -21,13 +43,41 @@ var albums = []album{
 }
 
 func main() {
+	db, err := sqlx.Connect("postgres", "host=localhost port=5433 user=postgres password=dbpass sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	db.MustExec(schema)
+
+	env := &Env{db: db}
 	router := gin.Default()
 	router.Use(cors.Default())
 	router.GET("/albums", getAlbums)
+	router.POST("/insertPerson", env.insertPerson)
 
 	router.Run("localhost:8080")
 }
 
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func (e *Env) insertPerson(c *gin.Context) {
+	_, err := e.db.NamedExec(`INSERT INTO person (first_name, last_name, email)
+        VALUES (:first_name, :last_name, :email)`,
+		map[string]interface{}{
+			"first_name": "John",
+			"last_name":  "Doe",
+			"email":      "kczuba@gmail.com",
+		},
+	)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Person inserted successfully"})
 }
